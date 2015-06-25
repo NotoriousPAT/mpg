7d65486058ffd8c8245a7bdca8cbc4cc73e9d39c
Patti MacKay <NotoriousPAT@users.noreply.github.com>
2015-06-25 15:55:42 -0400

    add views controllers and data

--- README.md
+++ README.md
@@ -1,4 +1,50 @@
-# gulp-base
+MPG
+Ever been car shopping? Ever wish that you could compare your prospective car's stats with really old cars? Well now you can. You are going to write an application that imports auto data and allows users to compare cars.
+
+Goals
+
+Write a single page application
+Write a responsive application
+Load data via AJAX
+Visualize data using C3 or other library
+Render data as HTML via templates
+The dataset
+
+There is a publicly available set of data about cars described here:
+
+A description of what the data means is available here
+The data itself is available here
+It's part of UCI's awesome dataset repository
+
+Alternatively, you could use this dataset
+
+The program
+
+MVP
+
+Show a list of all cars to the user
+The user can select a car, and you'll show them a barchart (or other vizualization) indicating how their selected car's MPG compares to the overall average
+Normal mode
+
+MVP plust the following:
+
+Instead of showing users the entire list, show them the first N (maybe 10 or 20) cars
+Users should be able to search for a car, and you'll show them the matching results
+Hard mode
+
+Users should be able to paginate through the list of cars or search results
+Keep the list of cars around as an array (or in a database) and serve them up using Node
+Allow users to add new cars to the list of cars
+Allow users to visualize cars by various categories:
+vs cars with the same number of cylinders
+vs all cars
+Compare horsepower
+Compare MPG
+Compare acceleration
+
+
+
+gulp-base
 
 A base setup for gulp (to be updated as we go along)
 
@@ -25,7 +71,7 @@ You should be all set at this point!
 
 ## Building an app
 
-This gulp configuration uses: 
+This gulp configuration uses:
 
 - Browserify to bundle your source
   - Includes jQuery, underscore, backbone, and parsley
--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var del = require('del');
 var watch = require('gulp-watch');
 var sourcemaps = require('gulp-sourcemaps');
 
+
 // Common patterns used throughout the gulp configuration
 var src = {
   allHtml: './src/**/*.html',
@@ -24,11 +25,12 @@ var src = {
   allFont: './src/**/*.{ttf,woff,otf,eot}',
   allScss: './src/**/*.scss',
   allImg: './src/**/*.{jpg,png,svg,gif,ico}'
+  allData: './src/**/*.data'
 };
 
 // The default task is what runs when you type 'gulp' in the terminal
 gulp.task('default', ['clean'], function () {
-  return gulp.start('html', 'img', 'font', 'js:views', 'js:vendor', 'js', 'scss', 'watch', 'reload', 'serve');
+  return gulp.start('html', 'data', 'img', 'font', 'js:views', 'js:vendor', 'js', 'scss', 'watch', 'reload', 'serve');
 });
 
 // Serve is a name I made up. You could call it 'dostuff' or whatever.
@@ -58,11 +60,15 @@ gulp.task('watch', function () {
   watch(src.allScss, function () {
     gulp.start('scss');
   });
-  
+
   watch(src.allImg, function () {
     gulp.start('img');
   });
-  
+
+  watch(src.allData, function () {
+    gulp.start('data');
+  });
+
   watch(src.allFont, function () {
     gulp.start('font');
   });
@@ -83,6 +89,7 @@ gulp.task('deploy', function() {
 // Adding the CSS task
 gulp.task('scss', function () {
   return gulp.src('./src/css/main.scss')
+    .on('error', swallowError)
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('main.css'))
     .pipe(autoprefixer({
@@ -94,7 +101,10 @@ gulp.task('scss', function () {
 
 // Build our JavaScript files using browserify
 gulp.task('js', function () {
-  return browserify('./src/js/init.js', { debug: true })
+  var stream;
+
+  try {
+    stream = browserify('./src/js/init.js', { debug: true })
     .transform('bulkify')
     .transform({ global: true }, 'uglifyify')
     .external('views')
@@ -102,7 +112,14 @@ gulp.task('js', function () {
     .external('underscore')
     .external('backbone')
     .external('parsleyjs')
-    .bundle()
+    .bundle();
+  } catch (ex) {
+    console.error(ex);
+    return;
+  }
+
+  return stream
+     .on('error', swallowError)
     .pipe(source('app.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({ loadMaps: true }))
@@ -153,6 +170,12 @@ gulp.task('img', function () {
     .pipe(gulp.dest('./dist'));
 });
 
+// Move any images to the dist folder
+gulp.task('data', function () {
+  return gulp.src(src.allData)
+    .pipe(gulp.dest('./dist'));
+});
+
 // Move any fonts to the dist folder
 gulp.task('font', function () {
   return gulp.src(src.allFont)
@@ -163,3 +186,9 @@ gulp.task('font', function () {
 gulp.task('clean', function (cb) {
   del('./dist', cb);
 });
+
+// Prevent gulp from crashing and leaving a running Node process behind
+function swallowError (error) {
+  console.log(error.toString());
+  this.emit('end');
+}
